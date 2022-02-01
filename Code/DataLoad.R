26e5ee59c84c71d8bbaeb2cbf98f123b2ef2a619
alfell <ale_feller@hotmail.com>
2022-02-01 14:01:29 +0100

    getURL for ignoring SSL certificate

--- Code/DataLoad.R
+++ Code/DataLoad.R
@@ -3,62 +3,68 @@ library(tidyverse)
 
 ###Load Data
 ##Prices and sale volumes by city neighborhood and zone type
-zones <- read.csv(url("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau514od5141/download/BAU514OD5141.csv"), 
-                  stringsAsFactors = FALSE, 
-                  sep = ",",
-                  encoding = "UTF-8")
+zonesURL <- getURL("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau514od5141/download/BAU514OD5141.csv",
+									 .opts = list(ssl.verifypeer = FALSE)
+)
+zones <- read.csv(textConnection(zonesURL), 
+									header = TRUE,
+									encoding = "UTF-8")
 
 ##Price series and sales volumes by city neighborhood and zone type
-series <- read.csv(url("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau514od5142/download/BAU514OD5142.csv"), 
-                   stringsAsFactors = FALSE, 
-                   sep = ",",
-                   encoding = "UTF-8")
+seriesURL <- getURL("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau514od5142/download/BAU514OD5142.csv",
+										.opts = list(ssl.verifypeer = FALSE)
+)
+series <- read.csv(textConnection(seriesURL), 
+									 header = TRUE,
+									 encoding = "UTF-8")
 
 ##All Addresses
-addresses <- read.csv(url("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau_hae_lima_zuordnung_adr_quartier_bzo16_bzo99/download/BAU514OD5143.csv"), 
-                      stringsAsFactors = FALSE, 
-                      sep = ",",
-                      encoding = "UTF-8")
+addressesURL <- getURL("https://data.integ.stadt-zuerich.ch/dataset/int_dwh_archiv_bau_hae_lima_zuordnung_adr_quartier_bzo16_bzo99/download/BAU514OD5143.csv",
+											 .opts = list(ssl.verifypeer = FALSE)
+)
+addresses <- read.csv(textConnection(addressesURL), 
+											header = TRUE,
+											encoding = "UTF-8")
 
 ###Data Transformation
 ##Zones
 zones <- zones %>% 
-  rename(Typ = X.U.FEFF.Typ)
-  
+	rename(Typ = X.U.FEFF.Typ)
+
 
 
 ##BZO16
 zonesBZO16 <- zones %>% 
-  filter(BZO == "BZO16") %>% 
-  rename(Total = ALLE,
-         Z = ZE,
-         K = KE,
-         Q = QU,
-         W2 = W2,
-         W3 = W23,
-         W4 = W34,
-         W5 = W45,
-         W6 = W56)
+	filter(BZO == "BZO16") %>% 
+	rename(Total = ALLE,
+				 Z = ZE,
+				 K = KE,
+				 Q = QU,
+				 W2 = W2,
+				 W3 = W23,
+				 W4 = W34,
+				 W5 = W45,
+				 W6 = W56)
 
 ##BZO99
 zonesBZO99 <- zones %>% 
-  filter(BZO == "BZO99") %>% 
-  rename(Total = ALLE,
-         Z = ZE,
-         K = KE,
-         Q = QU,
-         ` ` = W2,
-         W2 = W23,
-         W3 = W34,
-         W4 = W45,
-         W5 = W56)
+	filter(BZO == "BZO99") %>% 
+	rename(Total = ALLE,
+				 Z = ZE,
+				 K = KE,
+				 Q = QU,
+				 ` ` = W2,
+				 W2 = W23,
+				 W3 = W34,
+				 W4 = W45,
+				 W5 = W56)
 
 ##Addresses
 addresses <- addresses %>% 
-  rename(Strasse = X.U.FEFF.Strasse) %>% 
-  mutate(Zones = case_when(ZoneBZO16 == ZoneBZO99 ~ paste(ZoneBZO16),
-                           TRUE ~ paste0(ZoneBZO16, " (bis 2018: ", ZoneBZO99,")")))
+	rename(Strasse = X.U.FEFF.Strasse) %>% 
+	mutate(Zones = case_when(ZoneBZO16 == ZoneBZO99 ~ paste(ZoneBZO16),
+													 TRUE ~ paste0(ZoneBZO16, " (bis 2018: ", ZoneBZO99,")")))
 
 ##Series
 series <- series %>% 
-  rename(Typ = X.U.FEFF.Typ)
+	rename(Typ = X.U.FEFF.Typ)
